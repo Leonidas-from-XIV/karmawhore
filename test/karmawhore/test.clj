1f4fa2a57745b0913cee4365043c69b3500af8de
Marek Kubica <marek@xivilization.net>
2010-12-26 09:42:42 +0100

    Added some more unittest test cases

--- test/karmawhore/test.clj
+++ test/karmawhore/test.clj
@@ -2,5 +2,18 @@
   (:use [karmawhore] :reload)
   (:use [clojure.test]))
 
-(deftest parses-simple-votes
-  (is {"Leonidas" 1} (get-upvotes "hey, Leondidas++ for writing Karmawhore")))
+;(def a {"Leonidas" {:upvotes 1 :downvotes 2}})
+;(def b {"Leonidas" {:upvotes 4 :downvotes 1}})
+;(merge-with (partial merge-with +) a b)
+(deftest parses-votes
+  (is {"Leonidas" 1} (get-upvotes "hey, Leondidas++ for writing Karmawhore"))
+  (is {"Leonidas" 2} (get-upvotes "Leonidas++ gets a lot of karma, Leonidas++"))
+  (is {"Leonidas" 1} (get-upvotes "Xenefungus-- gets no karma but Leonidas++ does"))
+  (is {} (get-upvotes "Noone gets karma"))
+  (is {} (get-upvotes "URLs like http://in.tum.de/?foo=bar++baz don't have karma"))
+  (is {"Xenefungus" 1} (get-downvotes "Xenefungus-- uses Windows"))
+  (is {} (get-downvotes "Everyone is fine")))
+
+(deftest regex-matches
+  (is (["Leonidas++" "Leonidas"] "Simple upvote for Leonidas++"))
+  (is (["Leonidas--" "Leonidas"] "Equally simple Leonidas-- downvote")))
