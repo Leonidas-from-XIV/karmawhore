e2ecfc195da3d84f531c855f4c13c30c92265d46
Marek Kubica <marek@xivilization.net>
2011-01-26 22:46:45 +0100

    Added a test to try merging nicks

--- test/karmawhore/test.clj
+++ test/karmawhore/test.clj
@@ -30,6 +30,22 @@
          {"larsrh" {:upvotes 1 :downvotes 0}}
          "Edeltraudzombiexenefungus-- gets no karma, larsrh++ does")))
 
+(deftest joining-nicks
+  (binding [config {:join {"Xenefungus" #{#"Zombiexenefungus" #"Edeltraudzombiexenefungus"}
+                           "x127" #{".127"}}}]
+    (are [parsed line] (= parsed (get-votes line))
+         {"Xenefungus" {:upvotes 1 :downvotes 0}}
+         "Zombiexenefungus++ gets karma for zombie prefix"
+
+         {"x127" {:upvotes 2 :downvotes 0}}
+         "x127++ and {127++ is actually the same"
+
+         {"Xenefungus" {:upvotes 0 :downvotes 1} "x127" {:upvotes 1 :downvotes 0}}
+         "Edeltraudzombiexenefungus-- for long nick, {127++ for strange characters"
+
+         {"x127" {:upvotes 1 :downvotes 1}}
+         "x127++ for short nick, {127-- for two nicks in channel")))
+
 (deftest regex-matches
   (are [matched line] (= matched (match-line line))
        '(["Leonidas++" "Leonidas" "++"]) "Simple upvote for Leonidas++"
