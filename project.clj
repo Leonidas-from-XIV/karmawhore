c8ca76981bdd6b87697de5890e4bac13f5546350
Marek Kubica <marek@xivilization.net>
2011-02-02 00:55:35 +0100

    Post-release version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject karmawhore "0.2.4"
+(defproject karmawhore "0.2.5"
   :description "Karma tracker for IRC channels"
   :main karmawhore.parser
   :dependencies [[org.clojure/clojure "1.2.0"]
