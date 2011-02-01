669c9fe02d510578fc2718c2091c52ac25c1ba8c
Marek Kubica <marek@xivilization.net>
2011-02-02 00:53:13 +0100

    Nicer HTML output. Now with \infty percent more CSS.

--- src/karmawhore/html.clj
+++ src/karmawhore/html.clj
@@ -4,23 +4,32 @@
 (defn record-row [record]
   (let [[nick {u :upvotes d :downvotes s :sum}] record]
     [:tr
-     [:td.karmawhore-nick nick]
-     [:td.karmawhore-upvotes (str u)]
-     [:td.karmawhore-downvotes (str d)]
-     [:td.karmawhore-sum (str s)]]))
+     [:td.nick nick]
+     [:td.upvotes (str u)]
+     [:td.downvotes (str d)]
+     [:td.sum (str s)]]))
 
 (defn records-table [records]
   `[:table
+    [:tr
+     [:th "Nick"]
+     [:th "Upvotes"]
+     [:th "Downvotes"]
+     [:th "Sum"]]
     ~@(map record-row records)])
 
 (defn html-wrapper [records]
   [:html
    [:head
-    [:title "Karma statistics"]]
+    [:title "Karma statistics"]
+    [:link {:rel "stylesheet" :type "text/css" :href "karmawhore.css"}]]
    [:body
+    [:h2 "Users sorted by Karma"]
     [:p
      (records-table records)]
-    [:p "Powered by Karmawhore"]]])
+    [:p#powered-by
+     "Powered by "
+     [:a {:href "https://github.com/Leonidas-from-XIV/karmawhore"} "Karmawhore"]]]])
 
 (defn records->html [records]
   (html (html-wrapper records)))
